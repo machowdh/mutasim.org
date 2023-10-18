df8062d3260f540e5d9a2df869ee7cc15ffba54b
Mutasim Chowdhury <35827063+machowdh@users.noreply.github.com>
2023-10-18 00:02:23 -0500

    remove favicon + resume change

--- pages/_document.js
+++ pages/_document.js
@@ -16,24 +16,24 @@ class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicons/favicon.ico" />
+          {/* <link rel="icon" href="/favicons/favicon.ico" /> */}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/favicons/apple-touch-icon.png"
           />
-          <link
+          {/* <link
             rel="icon"
             type="image/png"
             sizes="32x32"
             href="/favicons/favicon-32x32.png"
-          />
-          <link
+          /> */}
+          {/* <link
             rel="icon"
             type="image/png"
             sizes="16x16"
             href="/favicons/favicon-16x16.png"
-          />
+          /> */}
           <link rel="manifest" href="/favicons/site.webmanifest" />
           <link
             rel="mask-icon"
